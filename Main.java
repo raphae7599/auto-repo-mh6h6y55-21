3b2f81eb7e176406e25375a958994154130deac3
JulioCezar9619 <julin15@hotmail.com>
2025-10-25 16:10:57 +0000

    Initial commit: java project

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * auto-repo-mh6h6y55-21 - Java Implementation
+ * A simple Hello World program
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to auto-repo-mh6h6y55-21");
+        
+        // Simple calculation
+        int[] numbers = {1, 2, 3, 4, 5};
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        System.out.println("Sum of numbers: " + total);
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# auto-repo-mh6h6y55-21
+
+A simple Java project.
+
+## Description
+This is an auto-generated project demonstrating basic java functionality.
+
+## Running
+```bash
+# See the main file: Main.java
+```
+
+## Language
+- Java
+
+## License
+MIT
